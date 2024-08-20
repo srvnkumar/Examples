96c91b1687f9b809dc3d4623e6421b60c06770ca
srvnkumar <v.saravana@gmail.com>
2024-08-20 23:53:32 +0530

    cleanup code

--- Sorting/src/main/java/com/sanguine/InsertionSort.java
+++ Sorting/src/main/java/com/sanguine/InsertionSort.java
@@ -2,18 +2,18 @@ package com.sanguine;
 
 public class InsertionSort {
 
-    public int[] insertionSort(int[] a){
-        int temp =0;
-        for(int i=0; i<a.length;i++){
-            for(int j=i+1; j<a.length;j++){
-                if(a[i]>a[j]){
+    public void insertionSort(int[] a) {
+        int temp;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
                     temp = a[i];
                     a[i] = a[j];
                     a[j] = temp;
                 }
             }
         }
-        return a;
+
     }
 
 
--- Sorting/src/test/java/com/sanguine/InsertionSortTest.java
+++ Sorting/src/test/java/com/sanguine/InsertionSortTest.java
@@ -6,9 +6,9 @@ class InsertionSortTest {
 
     @org.junit.jupiter.api.Test
     void insertionSort() {
-        int[] a ={1,3,4,2,6};
+        int[] a = {1, 3, 4, 2, 6};
         new InsertionSort().insertionSort(a);
-        int[] b = {1,2,3,4,6};
-        assertArrayEquals(b,a);
+        int[] b = {1, 2, 3, 4, 6};
+        assertArrayEquals(b, a);
     }
 }
