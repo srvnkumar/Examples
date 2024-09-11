66393401dc964556eb109fbff977bc50e4b20578
srvnkumar <v.saravana@gmail.com>
2024-09-11 23:27:47 +0530

    Added QuickSort

--- Sorting/src/main/java/com/sanguine/QuickSort.java
+++ Sorting/src/main/java/com/sanguine/QuickSort.java
@@ -2,38 +2,37 @@ package com.sanguine;
 
 import java.util.Arrays;
 
-import static java.util.Collections.swap;
-
 public class QuickSort {
 
     public static void main(String[] args) {
-        int a[] = {1,3,2,4,6,5};
-        quicksort(a,0,a.length-1);
+        int[] a = {1, 3, 2, 4, 6, 5};
+        quicksort(a, 0, a.length - 1);
         Arrays.stream(a).forEach(System.out::println);
     }
 
     private static void quicksort(int[] a, int low, int high) {
-        if(low < high){
+        if (low < high) {
             int pi = partition(a, low, high);
-            quicksort(a, low, pi-1);
-            quicksort(a,pi+1, high);
+            quicksort(a, low, pi - 1);
+            quicksort(a, pi + 1, high);
         }
     }
 
     private static int partition(int[] a, int low, int high) {
-         int piviot = a[high];
-         int i = low -1;
-         for(int j=low; j<high;j++){
-             if(a[j]< piviot){
-                 i++;
-                 swap(a,i,j);
-             }
-         }
-         swap(a,i+1,high);
-         return i+1;
+        int piviot = a[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (a[j] < piviot) {
+                i++;
+                swap(a, i, j);
+            }
+        }
+        swap(a, i + 1, high);
+        return i + 1;
 
     }
-    private static void swap(int[] a, int i, int j){
+
+    private static void swap(int[] a, int i, int j) {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
