6e32c13ddd5953ecc8fe9bc0d114bb9c660796aa
srvnkumar <v.saravana@gmail.com>
2024-09-16 23:30:03 +0530

    Added Problem Set

--- Problems/src/main/java/com/sanguine/Main.java
+++ Problems/src/main/java/com/sanguine/Main.java
@@ -1,29 +1,17 @@
 package com.sanguine;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    /*    preferences = {
-                0: [0, 1, 3, 6],
-        1: [1, 4, 7],
-        2: [2, 4, 7, 5],
-        3: [3, 2, 5],
-        4: [5, 8]*/
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        List<Integer> list1 = List.of(0, 1, 3, 6);
-        List<Integer> list2 = List.of(1, 4, 7);
-        List<Integer> list3 = List.of(2, 4, 7, 5);
-        List<Integer> list4 = List.of(3, 2, 5);
-        List<Integer> list5 = List.of(5, 8);
-        map.put(0, list1);
-        map.put(1, list2);
-        map.put(2, list3);
-        map.put(3, list4);
-        map.put(4, list5);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
     }
 }
