91cbda4aa525ec4cb3e2315adcf082f645b5d1ba
srvnkumar <v.saravana@gmail.com>
2024-10-28 23:16:15 +0530

    fixed compiler errors

--- Problems/src/main/java/com/sanguine/Main.java
+++ Problems/src/main/java/com/sanguine/Main.java
@@ -1,12 +1,16 @@
 package com.sanguine;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scannner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the input string:");
-        String input = sc.nextString();
+        String input = sc.next();
         System.out.println("Enter the substring length");
         int k = sc.nextInt();
         for(String s: getSubStrings(input, k))
@@ -15,7 +19,7 @@ public class Main {
         }
     }
 
-    public static void getSubStrings(String input, int k){
+    public static List<String> getSubStrings(String input, int k){
         List<String> result = new ArrayList<>();
         for (int i=0; i<input.length()-k+1; i++){
             result.add(input.substring(i,i+k));
