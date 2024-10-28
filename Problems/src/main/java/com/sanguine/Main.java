ae42e51697c47c758436e968a309252bb10d7847
srvnkumar <v.saravana@gmail.com>
2024-10-28 23:13:26 +0530

    k substring problem

--- Problems/src/main/java/com/sanguine/Main.java
+++ Problems/src/main/java/com/sanguine/Main.java
@@ -4,14 +4,22 @@ package com.sanguine;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scannner sc = new Scanner(System.in);
+        System.out.println("Enter the input string:");
+        String input = sc.nextString();
+        System.out.println("Enter the substring length");
+        int k = sc.nextInt();
+        for(String s: getSubStrings(input, k))
+        {
+            System.out.println(s);
         }
+    }
+
+    public static void getSubStrings(String input, int k){
+        List<String> result = new ArrayList<>();
+        for (int i=0; i<input.length()-k+1; i++){
+            result.add(input.substring(i,i+k));
+        }
+        return result;
     }
 }
