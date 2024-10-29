c5b470eaa8f4485203ae30ed1ad3cb417ab38ade
srvnkumar <v.saravana@gmail.com>
2024-10-29 22:54:16 +0530

    factorial will work with >=0 numbers

--- Problems/src/main/java/com/sanguine/Factorial.java
+++ Problems/src/main/java/com/sanguine/Factorial.java
@@ -14,7 +14,7 @@ public class Factorial {
     }
 
     public static int recursiveFact(int n) {
-        if(n<=1)
+        if(n==0)
             return 1;
         return n * recursiveFact(n-1);
     }
